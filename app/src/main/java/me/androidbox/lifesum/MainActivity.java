efb6e6a6f3cbab91c053cf5be4e07b11476922ae
Steve <steve1.rm@gmail.com>
2015-12-18 01:33:30 +0700

    Completed the reset of the layout with buttons and text

--- app/src/main/java/me/androidbox/lifesum/MainActivity.java
+++ app/src/main/java/me/androidbox/lifesum/MainActivity.java
@@ -1,5 +1,6 @@
 package me.androidbox.lifesum;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.fragment_container, new MainFragment(), "MainFragment");
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.commit();
+        }
     }
 }
---
+++ app/src/main/java/me/androidbox/lifesum/MainFragment.java
@@ -0,0 +1,30 @@
+package me.androidbox.lifesum;
+
+import android.app.Fragment;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        final TextView tvTitle = (TextView)view.findViewById(R.id.tvTitle);
+        Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(), "CalligraphyFLF.ttf");
+        tvTitle.setTypeface(typeface);
+
+        return view;
+    }
+}
